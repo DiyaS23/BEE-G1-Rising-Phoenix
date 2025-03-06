2e3d1657af7ae68a00402e7164d2cba9b046abe8
Diya <sabikhidiya@gmail.com>
2025-03-06 17:02:37 +0530

    Changes in file.js

--- BackendPro/file.js
+++ BackendPro/file.js
@@ -1,16 +1,13 @@
 const fs = require("fs");
-const path = require("path");
 function readData(filename) {
     try {
       const data = fs.readFileSync(`${filename}.json`, "utf8");
       return JSON.parse(data);
     } catch (error) {
       console.error(`Error reading ${filename}:`, error);
-      return []; // Return an empty array instead of crashing
+      return []; 
     }
   }
-
-
 function writeData(file, data) {
     fs.writeFileSync(`${file}.json`, JSON.stringify(data, null, 2));
 }
